34bbe56414e2fd8f4add579b366aaad9d52cbbb4
Daniel-bee <daniel123_86@live.com>
2021-11-10 22:30:15 +0300

    task3 updated with task4

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/Python3
 import uuid
-import datetime
+from datetime import datetime
 """BaseModel that defines all common attributes/methods for other classes:"""
 
 
@@ -9,12 +9,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialize imprtant instance attribute"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        """dictionary representation of an instance (method to_dict())."""
+
+        if 'errormessage' not in kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
 
     def save(self):
         """Update the current datetime"""
-        self.updated_at = datetime.datetime.today()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """Returns the dict format of an object"""

a6aedce7765ef306ef67827a1feb35a56ef08f68
Daniel-bee <daniel123_86@live.com>
2021-11-11 17:54:37 +0300

    task3 updated

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialize imprtant instance attribute"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         """dictionary representation of an instance (method to_dict())."""
 
         if len(kwargs) != 0:
@@ -32,11 +32,11 @@ class BaseModel:
 
     def to_dict(self):
         """Returns the dict format of an object"""
-        kvdict = self.__dict__
-        strtf = "%Y-%m-%dT%H:%M:%S.%f"
+        kvdict = self.__dict__.copy()
+        #strtf = "%Y-%m-%dT%H:%M:%S.%f"
         kvdict["__class__"] = type(self).__name__
-        kvdict["updated_at"] = self.updated_at.strftime(strtf)
-        kvdict["created_at"] = self.created_at.strftime(strtf)
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
         return kvdict
 
     def __str__(self):

59e533ba3e72c5d81ce77dcb0bfd2c755c9ccbc5
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-11 13:01:12 +0000

    console entry point added with help EOF and quit commands

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+class HBNBCommand(cmd.Cmd):
+    intro = 'Welcome to the AirBnb Clone Console\n'
+    prompt = '(hbnb) '
+    ''' Entry point of console program and commands center '''
+
+    def emptyline(arg):
+        pass
+
+    def do_EOF(self, arg): 
+        '''Quit command to exit the program
+        '''
+        return True
+
+    def do_quit(self, arg):
+        '''Quit command to exit the program
+        '''
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()

d712556602953ca2fd9a06759909565717eed173
Daniel-bee <daniel123_86@live.com>
2021-11-12 15:24:46 +0300

    test amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import models
+import unittest
+from models.amenity import Amenity
+"""Test class Amenity"""
+
+
+class TestState(unittest.TestCase):
+    """Simple test case for State class"""
+    def test_attr(self):
+        attr = hasattr(Amenity, 'name')
+        self.assertEqual(attr, True) 
+
+    def test_type(self):
+        """Test the type of State public attr"""
+        type_ = type(Amenity.name).__name__
+        self.assertEqual(type_, "str")
+
+if __name__ == "__main__":
+    unittest.main()

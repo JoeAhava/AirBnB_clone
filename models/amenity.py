b1bbafebe9040dd6ac0b7da1d605c55048d6519e
Daniel-bee <daniel123_86@live.com>
2021-11-12 09:34:04 +0300

    Amenity sub class created

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Amenity class that inherit form Basemodel"""
+
+
+class Amenity(BaseModel):
+    """Simple Amenity model"""
+    name = ""

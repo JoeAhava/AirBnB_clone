b3c09afa40661ce8cdca0559fd366e3fdac5f099
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-12 08:21:52 +0000

    added create and show command

--- console.py
+++ console.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+from models import storage
 import cmd
 class HBNBCommand(cmd.Cmd):
     intro = 'Welcome to the AirBnb Clone Console\n'
     prompt = '(hbnb) '
     ''' Entry point of console program and commands center '''
+
+    def do_create(self, arg):
+        if self.validate_class(arg):
+            base_model = BaseModel()
+            base_model.save()
+            print(base_model.id)
+
+    def do_show(self, arg):
+        args = arg.split()
+        if self.validate_class(args[0]):
+            if not ".".join(args) in storage.all().keys():
+                print("Not Found")
+            else:
+                print(storage.all()[".".join(args)])
 
     def emptyline(arg):
         pass
@@ -18,6 +34,16 @@ class HBNBCommand(cmd.Cmd):
         '''Quit command to exit the program
         '''
         return True
+    
+    def validate_class(self, arg):
+        if len(arg) is 0:
+            print("** class name missing **")
+            return False
+        elif not arg == type(BaseModel()).__name__:
+            print("** class doesn't exist **")
+            return False
+        else:
+            return True
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!usr/bin/python3
+from models.base_model import BaseModel
 import models
 import json
 """class FileStorage that serializes instances to a

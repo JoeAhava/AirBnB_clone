d32a81946a6b8974a924609b9b49741a06831bb2
Yohannes Terfa <lightshadow011@gmail.com>
2021-11-14 20:50:31 +0000

    fix

--- console.py
+++ console.py
@@ -55,7 +55,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
+        """Default behavior for cmd module when input is invalid
+        """
         argdict = {
             "all": self.do_all,
             "show": self.do_show,

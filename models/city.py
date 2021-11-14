d1d5907fc981b661993ee0298ac54291e4193463
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 13:33:12 +0000

    pep8 for city

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    
+    """ City class """
+
     state_id = ""
     name = ""
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-
-""" BaseModel that defines all common attributes/methods for other classes """

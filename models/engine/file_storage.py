1a9e3f7c8af2790b6faf0dbe91b0f4906fa20193
Daniel-bee <daniel123_86@live.com>
2021-11-14 12:17:15 +0300

    file storage cllass updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
-from models.base_model import BaseModel
 import models
 import json
+from models.base_model import BaseModel
 from models.state import State
 from models.city import City
 from models.place import Place

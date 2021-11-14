48bcd6e1bfa76c844ebf8d14c001a290990e4cc2
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-11-14 18:11:32 +0300

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,45 @@
-#!/usr/bin/python3
-''' module for BaseModel class '''
-from uuid import uuid4
-from datetime import datetime
-from . import storage
+#!/usr/bin/Python3
+"""BaseModel that defines all common attributes/methods for other classes:"""
 
+
+import models
+import uuid
+from datetime import datetime
 
 class BaseModel:
-    ''' class of the base model of higher-level data models '''
-    def __init__(self, *arg, **kwargs):
-        ''' BaseModel constructor '''
-        if kwargs:
-            for k in kwargs:
-                if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
-                elif k != '__class__':
-                    setattr(self, k, kwargs[k])
+    """Simple Base Model class"""
+    def __init__(self, *args, **kwargs):
+        """initialize imprtant instance attribute"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        """dictionary representation of an instance (method to_dict())."""
+
+        if len(kwargs) != 0:
+            strft = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        self.__dict__[key] = datetime.strptime(value, strft)
+                    else:
+                        self.__dict__[key] = value
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at.replace()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
-        ''' saves a model '''
-        self.updated_at = datetime.utcnow()
-        storage.save()
+        """Update the current datetime"""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        ''' returns a dictionary representation of the model '''
-        dct = self.__dict__.copy()
-        dct['__class__'] = self.__class__.__name__
-        dct['created_at'] = self.created_at.isoformat()
-        dct['updated_at'] = self.updated_at.isoformat()
-        return dct
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__.copy()
+        # strtf = "%Y-%m-%dT%H:%M:%S.%f"
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
 
     def __str__(self):
-        ''' returns a string representation of the model '''
-        return '[{}] ({}) {}'.format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"

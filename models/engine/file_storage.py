445da280c3a9516a9859a467affa202d622e16a6
Daniel-bee <daniel123_86@live.com>
2021-11-14 12:20:24 +0300

    file storage cllass updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,14 @@
 #!usr/bin/python3
 import json
+"""
 from models.base_model import BaseModel
 from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-"""class FileStorage that serializes instances to a
+
+class FileStorage that serializes instances to a
 JSON file and deserializes JSON file to instances
 """
 

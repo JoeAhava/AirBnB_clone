ebabe774ef829a56d8bfd6b5d68143355988fb48
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-11-14 17:35:43 +0300

    base class

--- models/base_model.py
+++ models/base_model.py
@@ -1,73 +1,44 @@
-#!/usr/bin/python3
-"""
-Custom base class for the entire project
-"""
-
-from uuid import uuid4
-from datetime import datetime
+#!/usr/bin/Python3
 import models
+import uuid
+from datetime import datetime
+"""BaseModel that defines all common attributes/methods for other classes:"""
+
 
 class BaseModel:
-    """Custom base for all the classes in the AirBnb console project
-    Arttributes:
-        id(str): handles unique user identity
-        created_at: assigns current datetime
-        updated_at: updates current datetime
-    Methods:
-        __str__: prints the class name, id, and creates dictionary
-        representations of the input values
-        save(self): updates instance arttributes with current datetime
-        to_dict(self): returns the dictionary values of the instance obj
-    """
-
+    """Simple Base Model class"""
     def __init__(self, *args, **kwargs):
-        """Public instance artributes initialization
-        after creation
-        Args:
-            *args(args): arguments
-            **kwargs(dict): attrubute values
-        """
-        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key in ("updated_at", "created_at"):
-                    self.__dict__[key] = datetime.strptime(
-                        value, DATE_TIME_FORMAT)
-                elif key[0] == "id":
-                    self.__dict__[key] = str(value)
-                else:
-                    self.__dict__[key] = value
+        """initialize imprtant instance attribute"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        """dictionary representation of an instance (method to_dict())."""
 
-    def __str__(self):
-        """
-        Returns string representation of the class
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        if len(kwargs) != 0:
+            strft = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        self.__dict__[key] = datetime.strptime(value, strft)
+                    else:
+                        self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """
-        Updates the public instance attribute:
-        'updated_at' - with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
+        """Update the current datetime"""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Method returns a dictionary containing all 
-        keys/values of __dict__ instance
-        """
-        map_objects = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                map_objects[key] = value.isoformat()
-            else:
-                map_objects[key] = value
-        map_objects["__class__"] = self.__class__.__name__
-        return map_objects
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__.copy()
+        # strtf = "%Y-%m-%dT%H:%M:%S.%f"
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
+
+    def __str__(self):
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
